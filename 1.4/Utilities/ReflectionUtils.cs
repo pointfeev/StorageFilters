7afe7253fae0de011bc907583755719ac7ba5b3c
pointfeev <zehmeen@gmail.com>
2022-11-25 14:14:17 -0500

    fix

--- 1.4/Utilities/ReflectionUtils.cs
+++ 1.4/Utilities/ReflectionUtils.cs
@@ -8,28 +8,10 @@ namespace StorageFilters.Utilities
         internal static bool IsMethodInCallStack(MethodBase method)
         {
             StackFrame[] stackFrames = new StackTrace().GetFrames();
-            // 0 = current method
-            // 1 = calling method
-            // 2+ = methods we want to check
             if (stackFrames.Length > 2)
-                for (int i = 2; i <= stackFrames.Length; i++)
-                {
-                    StackFrame stackFrame = stackFrames[i];
-                    if (stackFrame is null)
-                    {
-                        continue;
-                    }
-
-                    MethodBase stackMethod = stackFrame.GetMethod();
-                    if (stackMethod is null)
-                    {
-                        continue;
-                    }
-                    if (stackMethod == method)
-                    {
+                for (int i = 2; i < stackFrames.Length; i++)
+                    if (stackFrames[i].GetMethod() == method)
                         return true;
-                    }
-                }
             return false;
         }
     }
